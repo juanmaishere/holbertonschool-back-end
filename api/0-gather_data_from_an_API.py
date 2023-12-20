47ef0259795c2bef8f7dc2a9b1ce2b9de26962f4
juanmaishere <rsjuanmanuel@hotmail.com>
2023-12-20 13:31:35 -0300

    feat

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,19 +2,22 @@
 """
 Summary : Api requesting employee information
 """
+import json
 import requests
 from sys import argv
-import json
 
 if __name__ == "__main__":
     """ Module for Asking apis info"""
     employee_id = argv[1]
-    url_todos = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos" 
-    url_user = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response = requests.get(url_todos)
-    responseuser = requests.get(url_user)
+    url_t = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    url_u = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url_t)
+    responseuser = requests.get(url_u)
     sum = 0
 
+    if response.status_code >= 400 and responseuser.status_code >= 400:
+        print("Error fetching data")
+        exit()
     if response.status_code >= 400 and responseuser.status_code >= 400:
         print("Error fetching data")
         exit()
@@ -22,9 +25,9 @@ if __name__ == "__main__":
     employeeinfo = responseuser.json()
     name = employeeinfo['name']
     for task in employee:
-        if task['completed'] == True:
+        if task['completed'] is True:
             sum += 1
     print(f"Employee {name} is done with tasks({sum}/{len(employee)}):")
     for task in employee:
-        if task['completed'] == True:
+        if task['completed'] is True:
             print(f"\t {task['title']}")
